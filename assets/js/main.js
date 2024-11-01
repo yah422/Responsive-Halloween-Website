f712406b028007f14ebe2e0089444a21a39ef531
AsmaSAIDI <saidiasma422@gmail.com>
2024-11-01 22:31:54 +0100

    📦 NEW: Add all the different elements in the js and css part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,46 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('#nav-menu'),
+    navToggle = document.querySelector('#nav-toggle'),
+    navClose = document.querySelector('#nav-close')
 
+/* Menu show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu = document.querySelector('#nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ATROPOS JS ===============*/
+const myAtropos = Atropos({
+    el: '.home__images',
+    //rest of parameters
+    shadow: false,
+    highlight: false,
 
+})
 
 /*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.querySelector('#header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
